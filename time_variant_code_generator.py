8fc29b323d912c63ea791519fbd717ac899fb6d7
Lukas <74401119+Luki248@users.noreply.github.com>
2021-11-10 09:32:39 +0100

    added xor of time and password, print time until new calculation

--- time_variant_code_generator.py
+++ time_variant_code_generator.py
@@ -30,32 +30,43 @@ def generate_hash(input):
 
 def get_password_and_compare():
     h = hashlib.sha256()
-    h.update(getpass.getpass("Passwort: ").encode())
+    #h.update(getpass.getpass("Passwort: ").encode())
+    h.update("12345".encode())
     hash1 = h.hexdigest()
     h = hashlib.sha256()
     h.update(hard_coded_password.encode())
     hash2 = h.hexdigest()
-    return hash1 == hash2
+    return hash1 == hash2, hash1
+
+
+def make_xor(hash1, hash2):
+    hash3 = hex(int(hash1, 16) ^ int(hash2, 16))
+    return hash3[2:]
 
 
 if __name__ == "__main__":
 
-    #if get_password_and_compare():
+    password_compared, hash_pswd = get_password_and_compare()
     if True:
         clear()
 
         time_f = int(time.time())
         time_new = time_f - (time_f % 10)
-        hash = generate_hash(str(time_new).encode())
-        print(time_new, hash)
+        hash_time = generate_hash(str(time_new).encode())
+        time_struct = time.gmtime(time_new)
+        print(time.asctime(time_struct), make_xor(hash_time, hash_pswd))
+
 
         while True:
             time_f = int(time.time())
 
             if time_f % 10 == 0:
-                hash = generate_hash(str(time_f).encode())
-                print(time_f, hash)
+                hash_time = generate_hash(str(time_f).encode())
+                time_struct = time.gmtime(time_f)
+                clear()
+                print(time.asctime(time_struct), make_xor(hash_time, hash_pswd))
             
+            print("\r", 9 - (time_f % 10), " ", end="")
             time.sleep(1)
 
     else:
